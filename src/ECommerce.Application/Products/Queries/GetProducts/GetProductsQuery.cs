754f298463ad184f71e3af4a6e421c077af2805d
Huong Tang <huong.tang@swisscom.com>
2026-04-24 14:08:44 +0700

    feat(cache): mark product queries as ICacheable, register pipeline behaviors

--- src/ECommerce.Application/Products/Queries/GetProducts/GetProductsQuery.cs
+++ src/ECommerce.Application/Products/Queries/GetProducts/GetProductsQuery.cs
@@ -1,6 +1,11 @@
+using ECommerce.Application.Caching;
 using ECommerce.Application.Common.Dtos;
 using MediatR;
 
 namespace ECommerce.Application.Products.Queries.GetProducts;
 
-public sealed record GetProductsQuery : IRequest<IReadOnlyList<ProductDto>>;
+public sealed record GetProductsQuery : IRequest<IReadOnlyList<ProductDto>>, ICacheable
+{
+    public string CacheKey => "products:all";
+    public TimeSpan CacheDuration => TimeSpan.FromMinutes(2);
+}
